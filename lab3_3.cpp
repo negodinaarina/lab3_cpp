f16f4c2d2a39a9178675f02e51bc1320872bc5e5
Arina Negodina <66848459+negodinaarina@users.noreply.github.com>
2023-09-16 11:58:26 +0300

    Update lab3_3.cpp

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,10 +1,11 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
+// функция поиска площади равностороннего треугольник
 double find_square(int a){
     return sqrt(3)*a*a/4;
 }
-
+// функция поиска площади разностороннего треугольника
 double find_square(int a, int b, int c){
     double p = (a + b + c) / 2;
     return sqrt(p*(p-a)*(p-b)*(p-c));
@@ -12,6 +13,7 @@ double find_square(int a, int b, int c){
 
 int main(){
     bool triangle_type;
+    //Просим выбрать тип треугольника и ввести стороны
     cout << "If triangle is equilateral - type in 1, else - 0";
     cin >> triangle_type;
     if(triangle_type){
