810e741738a5d7e097d40a0d7b025dc645d0aa9f
Arina Negodina <66848459+negodinaarina@users.noreply.github.com>
2023-09-16 12:04:55 +0300

    Update lab3_4.cpp

--- lab3_4.cpp
+++ lab3_4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// Рекурсивная функция поиска суммы ряда
 int recursion_func(int n){
     if (n == 1){
         return 5;
@@ -7,8 +8,13 @@ int recursion_func(int n){
     return 5*n + recursion_func(n-1);
 }
 int main(){
+    // Просим вести число, инициализируем int n,  проверяем, чтоб число было больше нуля.
     int n;
     cout << "Enter n: ";
     cin >> n;
+    if(n<=0){cout << "n must be more than zero";}
+    else{
     cout << recursion_func(n);
+    }
+    return 0;
 }
